789360a50c66e0966e4ea75f436ba73ee5b066ca
shyamshiva <shyamshiva.g@gmail.com>
2016-03-23 08:59:23 +0530

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,50 @@
-include nginx
+class nginx{
+package{ 'nginx':
+  ensure => present,
+  }
+  
+  file{ '/var/www':
+  ensure => directory,
+  owner => 'root',
+  group => 'root',
+  mode => '0777',
+  }
+  
+  file{ '/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0777',
+    source => 'puppet:///modules/nginx/index.html';
+    }
+    
+    file { '/etc/nginx/nginx.conf':
+      ensure => file,
+      owner => 'root',
+      group => 'root',
+      source => 'puppet:///modules/nginx/nginx.conf',
+      require => Package['nginx'],
+      notify => Service['nginx'],
+      }
+      
+      file	{	'/etc/nginx/conf.d':
+          ensure	=>	directory,
+          owner		=>	'root',
+          group		=>	'root',
+          mode			=>	'0777',
+      }
+      file	{	'/etc/nginx/conf.d/default.conf':
+        ensure		=>	file,
+        owner			=>	'root',
+        group			=>	'root',
+        mode				=>	'0777',
+        source		=>	'puppet:///modules/nginx/default.conf',
+        require	=>	Package['nginx'],
+        notify		=>	Service['nginx'],
+      }
+      service	{	'nginx':
+        ensure				=>	running,
+        enable				=>	true,
+      }
+    
+    }
