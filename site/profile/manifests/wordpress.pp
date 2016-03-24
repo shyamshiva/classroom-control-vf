1ef2cb513a0fc70107184c692d3977e2fac428dd
shyamshiva <shyamshiva.g@gmail.com>
2016-03-24 14:48:14 +0530

    Update wordpress.pp

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,5 +1,19 @@
 class profile::wordpress {
 
+
+
+#mysql
+class { '::mysql::server':
+  root_password           => 'strongpassword',
+}
+
+#apache
+class { 'apache': }
+
+#php
+
+#wordpress
+class { 'wordpress': }
  # TODO
 
 }
