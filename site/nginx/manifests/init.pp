260494ff4fdf61b09a969df03b0ae87b2aae223b
shyamshiva <shyamshiva.g@gmail.com>
2016-03-24 13:22:45 +0530

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -42,7 +42,6 @@ ensure	=>	directory,
 file	{	"${docroot}/index.html":
 ensure	=>	file,
 source	=>	"puppet:///modules/nginx/$firstfile",
-notify => {"$firstfile"},
 }
 file	{	"${confdir}/nginx.conf":
 ensure		=>	file,
